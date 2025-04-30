de20f8c3ccbfac27f070e12febe46290a732ccb9
ftomurad <b00158351@mytudublin.ie>
2025-04-30 21:08:49 +0100

    Create index.php
    for testing header

---
+++ fv/public/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once '../template/header.php'; 
+require_once '../loginstatus.php';
+?>
+
+<br><br><br><br><br><br><br><br><br><br>
+
+<?php require_once '../template/footer.php'; ?>
