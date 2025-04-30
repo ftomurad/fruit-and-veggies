5f6b55362431c9fec76147ec27d3469020048e0b
ftomurad <b00158351@mytudublin.ie>
2025-04-30 21:11:46 +0100

    Create footer.php
    finished footer

---
+++ fv/template/footer.php
@@ -0,0 +1,56 @@
+</div> <!-- end of .content from header.php -->
+
+<!-- Footer -->
+<footer class="site-footer">
+
+  <div class="footer-top">
+    
+    <!-- LEFT Logo -->
+    <div class="footer-col">
+      <img src="../images/logo.png" alt="Fruit and Veggies Logo" class="footer-logo">
+      <p>Not a member?</p>
+      <a href="register.php" class="join-button">Join Us</a>
+    </div>
+
+    <!-- MIDDLE Links -->
+    <div class="footer-col">
+      <h4>Quick Links</h4>
+      <ul class="footer-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="shop.php">Online Store</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+
+    <!-- RIGHT Social -->
+    <div class="footer-col">
+      <h4>Stay Connected</h4>
+      <ul class="footer-links">
+        <li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
+        <li><a href="https://www.instagram.com" target="_blank">Instagram</a></li>
+        <li><a href="https://www.twitter.com" target="_blank">Twitter</a></li>
+      </ul>
+    </div>
+    
+  </div>
+
+  <!-- Bottom part -->
+  <div class="footer-bottom">
+    <div class="footer-bottom-left">
+      <p>&copy; 2025 Fruit &amp; Veggies. All rights reserved.</p>
+    </div>
+    <div class="footer-bottom-right">
+      <a href="#">Privacy Policy</a>
+      <span>|</span>
+      <a href="#">Terms Of Service</a>
+      <span>|</span>
+      <a href="#">Cookies</a>
+    </div>
+  </div>
+
+</footer>
+
+</div>
+</body>
+</html>
