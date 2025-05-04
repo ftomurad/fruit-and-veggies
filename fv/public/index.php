b0d15e469edf08e8d3ec2148ce425797c08a3941
ftomurad <b00158351@mytudublin.ie>
2025-05-04 20:33:08 +0100

    Update index.php

--- fv/public/index.php
+++ fv/public/index.php
@@ -1,20 +1,6 @@
 <?php
-/**
- * HOMEPAGE
- * -------------------------------------------------
- * 1. Header
- * 2. Gets current username from loginstatus
- * 3. Echo a welcome
- * 4. Text and image display
- * 5. Decorative image in the middle
- * 6. Footer
- */
-
-require_once '../template/header.php';    // session_start amd navbar 
-require_once '../loginstatus.php';        // for getUsername()
-
-// capital first letter and rest lowercase
-$username = ucfirst(strtolower(getUsername()));
+require_once '../template/header.php';  // session_start
+require_once '../private/pr_index.php';  // php part
 ?>
 
 <!-- Welcome -->
